ea4f1974d1b3ece29c92ba3894716f9760f3aabc
KudryashovR <romkud55@gmail.com>
2024-08-28 22:46:25 +0300

    ADD: Habit validators

--- main/models.py
+++ main/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from users.models import CustomUser
@@ -13,12 +14,28 @@ class Habit(models.Model):
                                       related_name='related_habits', verbose_name='связанная привычка')
     frequency = models.PositiveIntegerField(default=1, verbose_name='периодичность', help_text='в днях')
     reward = models.CharField(max_length=255, null=True, blank=True, verbose_name='вознаграждение')
-    time_to_complete = models.DurationField(null=True, blank=True, verbose_name='время на выполнение',
+    time_to_complete = models.PositiveIntegerField(verbose_name='время на выполнение',
                                             help_text='секунд')
     is_public = models.BooleanField(default=False, verbose_name='признак публичности')
 
     def __str__(self):
         return self.action
+
+    def clean(self):
+        if self.related_habit and self.reward:
+            raise ValidationError(
+                "Вы не можете установить одновременно связанную привычку и вознаграждение. Должно быть заполнено только одно из полей.")
+
+        if self.is_pleasant_habit and (self.related_habit or self.reward):
+            raise ValidationError("Приятная привычка не может иметь связанной с ней привычки или награды.")
+
+        if self.frequency > 7:
+            raise ValidationError("Привычку нельзя выполнять реже, чем раз в 7 дней.")
+
+        if self.time_to_complete > 120:
+            raise ValidationError("Время выполнения не должно превышать 120 секунд.")
+
+        super().clean()
 
     class Meta:
         verbose_name = 'пивычка'
