80809a8616bc9df9cf3853bb410bf2f34476a507
KudryashovR <romkud55@gmail.com>
2024-08-28 23:10:11 +0300

    ADD: CRUD for Habit

--- main/views.py
+++ main/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from main.models import Habit
+from main.serializers import HabitSerializer
+
+
+class HabitListCreate(generics.ListCreateAPIView):
+    """
+    Представление для списка и создания объектов Habit.
+
+    Данный класс позволяет получать список всех объектов Habit, а также создавать новые объекты Habit.
+
+    Атрибуты:
+        queryset (QuerySet): Все объекты модели Habit.
+        serializer_class (Serializer): Класс сериализатора, который будет использован для сериализации и десериализации
+                                       данных.
+    """
+
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Представление для получения, обновления и удаления объекта Habit по его идентификатору.
+
+    Данный класс позволяет выполнять операции получения, обновления и удаления конкретного объекта Habit.
+
+    Атрибуты:
+        queryset (QuerySet): Все объекты модели Habit.
+        serializer_class (Serializer): Класс сериализатора, который будет использован для сериализации и десериализации
+                                       данных.
+    """
+
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
