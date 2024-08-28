acf389603621c4d3007e70962f0c143613394555
KudryashovR <romkud55@gmail.com>
2024-08-28 23:55:01 +0300

    ADD: token request and user register

---
+++ users/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from users.models import CustomUser
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для профиля пользователя.
+    """
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'password', 'phone', 'city', 'avatar', 'payment_history']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            phone=validated_data['phone'],
+            city=validated_data['city']
+        )
+
+        return user
--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,14 @@
-urlpatterns = [
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from main.apps import MainConfig
+from users.views import UserCreate
+
+app_name = MainConfig.name
+
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', UserCreate.as_view(), name='create_user'),
 ]
