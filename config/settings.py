b77807f4cc68bdbc0d5e7fafe84d7589f6af7ba8
KudryashovR <159808397+KudryashovR@users.noreply.github.com>
2024-09-18 03:47:16 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -128,13 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
-STATICFILES_DIRS=[
-    (os.path.dirname(BASE_DIR),"static"),
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
 ]
-
-STATIC_ROOT = (os.path.dirname(BASE_DIR),"static_files")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
